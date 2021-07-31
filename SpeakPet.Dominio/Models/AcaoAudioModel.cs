3361338c4b4031f8425e2f861425b8d589e0bf14
galileuveloso <galileu.veloso@hotmail.com>
2021-07-31 12:58:20 -0300

    criacao AcaoAudioModel

---
+++ SpeakPet.Dominio/Models/AcaoAudioModel.cs
@@ -0,0 +1,11 @@
+﻿using SpeakPet.Dominio.Enums;
+
+namespace SpeakPet.Dominio.Models
+{
+    public class AcaoAudioModel
+    {
+        public int Id { get; set; }
+        public AcaoEnum Acao { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
